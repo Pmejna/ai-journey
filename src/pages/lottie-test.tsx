6f6e9661082917a27285d675b1f3b91b22d4fc37
Przemyslaw Mejna <pmejna@gmail.com>
2022-10-21 23:34:56 +0200

    build(lotties): add lotties to the project (#7)

---
+++ src/pages/lottie-test.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next';
+
+import FourOFourView from 'views/404/404View';
+
+const LottieTest: NextPage = () => {
+  return (
+      <FourOFourView></FourOFourView>
+  )
+}
+
+export default LottieTest
