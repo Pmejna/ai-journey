9b470b37652ae3de6bbc39b877b2ec7be8507cee
Przemyslaw Mejna <pmejna@gmail.com>
2022-10-22 01:42:11 +0200

    feat(index): add types from generated schema

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,15 @@ import type { GetStaticProps, NextPage } from "next";
 
 import { apolloClient } from "lib/apolloClient";
 
+import type { HomePage } from "schema/generated";
+
 import { homePageQuery } from "queries/HomePage";
+
 import { HomeView } from "views/HomeView/HomeView";
 
+export type HomePageData = Required<HomePage>;
 export interface HomeProps {
-  pageData: any;
+  pageData: HomePageData;
 }
 
 const Home: NextPage<HomeProps> = ({ pageData }) => {
