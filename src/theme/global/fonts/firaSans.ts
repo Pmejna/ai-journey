fbda13c4257d4d408968e82a7e85b9e380f80a17
Przemyslaw Mejna <pmejna@gmail.com>
2023-01-04 20:15:11 +0100

    feat(_document file): add preloaded imports for typography, add emotion cache to document (#12)

--- src/theme/global/fonts/firaSans.ts
+++ src/theme/global/fonts/firaSans.ts
@@ -1,30 +1,30 @@
-import { css } from '@emotion/react';
+import { css } from "@emotion/react";
 
 export const FiraSans = css`
   @font-face {
-    font-family: 'Fira Sans';
-    src: url('/fonts/FiraSans-Light.ttf') format('trueType');
+    font-family: "Fira Sans";
+    src: url("/fonts/FiraSans/FiraSans-Light.ttf") format("trueType");
     font-weight: 300;
     font-style: italic;
     font-display: swap;
   }
   @font-face {
-    font-family: 'Fira Sans';
-    src: url('/fonts/FiraSans-Regular.ttf') format('trueType');
+    font-family: "Fira Sans";
+    src: url("/fonts/FiraSans/FiraSans-Regular.ttf") format("trueType");
     font-weight: 400;
     font-style: normal;
     font-display: swap;
   }
   @font-face {
-    font-family: 'Fira Sans';
-    src: url('/fonts/FiraSans-Medium.ttf') format('trueType');
+    font-family: "Fira Sans";
+    src: url("/fonts/FiraSans/FiraSans-Medium.ttf") format("trueType");
     font-weight: 500;
     font-style: normal;
     font-display: swap;
   }
   @font-face {
-    font-family: 'Fira Sans';
-    src: url('/fonts/FiraSans-Bold.ttf') format('trueType');
+    font-family: "Fira Sans";
+    src: url("/fonts/FiraSans/FiraSans-Bold.ttf") format("trueType");
     font-weight: 700;
     font-style: normal;
     font-display: swap;
--- src/theme/global/index.ts
+++ src/theme/global/index.ts
@@ -1,4 +1,5 @@
-export { global } from './global';
-export { normalize } from './normalize';
-export { FiraSans } from './fonts';
+export { global } from "./global";
+export { normalize } from "./normalize";
+export { FiraSans } from "./fonts";
+export { Lato } from "./fonts";
 // below we will export fonts we will use
