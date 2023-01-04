bade32ee76b9382afffa2b467b38ad3b276784b5
Przemyslaw Mejna <pmejna@gmail.com>
2023-01-04 20:37:23 +0100

    feat(react-three-fiber): add deps, test mesh and canvas in homepage

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,11 +3,11 @@ import type { FunctionComponent } from "react";
 import { StyledFooter } from "./Footer.styles";
 
 const Footer: FunctionComponent = () => {
-    return ( 
-        <StyledFooter>
-            <h3>AI Journey</h3>
-        </StyledFooter> 
-     );
-}
- 
+  return (
+    <StyledFooter>
+      <h3>AI Journey</h3>
+    </StyledFooter>
+  );
+};
+
 export default Footer;
